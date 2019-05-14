20d47b19b320abcbc0e5a5586ebabdb0b57be034
timthinkcube <tungting.wu@gmail.com>
2019-05-14 14:05:19 +0800

    [add] first interview version init

---
+++ json/data.js
@@ -0,0 +1,44 @@
+var DATA_TABLE = [
+  {
+    "id": 1,
+    "name": "股感考試中基金",
+    "reward": 50,
+    "closePrice": 17,
+    "specialStock": false
+  },
+  {
+    "id": 2,
+    "name": "股感發展中基金",
+    "reward": 60,
+    "closePrice": 87,
+    "specialStock": false
+  },
+  {
+    "id": 3,
+    "name": "股感徵才中基金",
+    "reward": 70,
+    "closePrice": 66,
+    "specialStock": true
+  },
+  {
+    "id": 4,
+    "name": "股感面試中基金",
+    "reward": 80,
+    "closePrice": 66,
+    "specialStock": false
+  },
+  {
+    "id": 5,
+    "name": "股感歡迎你基金",
+    "reward": 80,
+    "closePrice": 88,
+    "specialStock": true
+  },
+  {
+    "id": 6,
+    "name": "股感發大財基金",
+    "reward": 100,
+    "closePrice": 77,
+    "specialStock": true
+  },
+]
